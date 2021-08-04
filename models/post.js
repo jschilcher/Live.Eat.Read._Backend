f6fcd1f2c7e60cdd4bd4fec37e12ff69943fc598
Jenelle <jschilcher8@gmail.com>
2021-08-03 20:57:35 -0500

    Add post schema

---
+++ models/post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const postSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true, minlength: 5, maxlength: 50 },
+    text: { type: String, required: true, minlength: 3, maxlength: 1000 },
+    image: { type: String, required: true },
+    rating: {type: Number, required: true, default: 0},
+    like: { type: Number, required: true, default: 0 },
+    dateAdded: { type: Date, default: Date.now },
+  },
+);
+
+const Post = mongoose.model("post", postSchema);
+
+function validatePost(post) {
+  const Schema = Joi.object({
+    username: Joi.string().min(5).max(50).required(),
+    text: Joi.string().min(3).max(1000).required(),
+    image: Joi.string().required(),
+  });
+  return Schema.validate(post);
+}
+
+module.exports = {
+  Post: Post,
+  validatePost: validatePost,
+};
